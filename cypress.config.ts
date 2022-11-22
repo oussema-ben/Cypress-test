e74f370348c0c22861dc855a5d5c4ae4ba5c6627
oussama <oussama.bensaleh@siteflow.com>
2022-11-22 10:59:53 +0100

    ajout config cypress Dashboard

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,11 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     chromeWebSecurity: false,
+    projectId: "nvqu7q",
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  
 });
